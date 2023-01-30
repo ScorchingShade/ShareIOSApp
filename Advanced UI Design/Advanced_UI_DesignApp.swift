d676f4abf54fea0867f045171823f4f05d53a275
ScorchingShade <ankush.sharma.con@hotstar.com>
2023-01-30 18:23:34 +0530

    Initial Commit

---
+++ Advanced UI Design/Advanced_UI_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Advanced_UI_DesignApp.swift
+//  Advanced UI Design
+//
+//  Created by Ankush on 30/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Advanced_UI_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
